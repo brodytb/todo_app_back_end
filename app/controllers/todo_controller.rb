b72aeafedb03a508240eb17ddd3c5f580c6add9e
Brody Bebout <brody077@gmail.com>
2018-07-11 17:44:51 +0000

    despacito mmmmmmmmmmmmmmmmmmmmmmmmm

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,12 @@ class TodoController < ApplicationController
     def index
     end
     def show 
-        @todo_description = "take out trash" 
-        @pomodoro_estimate = 3
+        todo_id = "1"
+        if todo_id=='1'
+        @todo_description = "take out trash" @pomodoro_estimate = 3
+    
+        if todo_id=='5'
+        @todo_description = "make meme"
+        @todo_emstimate = 3
     end
-end 
+end  
