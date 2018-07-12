9d76f563c576ae0014fcbb44d6b62c350aee79d0
Brody Bebout <brody077@gmail.com>
2018-07-12 13:00:49 +0000

    final

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,12 +2,41 @@ class TodoController < ApplicationController
     def index
     end
     def show 
-        todo_id = "1"
-        if todo_id=='1'
-        @todo_description = "take out trash" @pomodoro_estimate = 3
-    
-        if todo_id=='5'
-        @todo_description = "make meme"
-        @todo_emstimate = 3
+        @todo_description = ""
+        @pomodoro_estimate = 0
+        todo_id = params[:id]
+        if todo_id =='1'
+            @todo_description = "clean bed"
+            @pomodoro_estimate = 3
+        elsif todo_id =='2'
+            @todo_description = "clean house"
+            @pomodoro_estimate = 3
+        elsif todo_id =='3'
+            @todo_description = "make eggs" 
+            @pomodoro_estimate = 3
+       
+        end 
+                
+        @todo= Todo.find_by_id(params[:id])
     end
-end  
+    
+    def new 
+
+        
+    end
+    
+    def create
+        t=Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate=params[pomodoro_estimate]
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+        @todo= Todo.find_by_id(params[:id])
+    end
+    
+    def edit
+        
+        
+    end
+    
+end
